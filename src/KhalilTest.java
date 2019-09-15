196cfd195bbe012298f87228fc4ae720933b917a
Othmane ERRARHBI <othmane.errarhbi@deveryware.com>
2019-09-15 13:10:14 +0200

    Asing Merge request

--- src/KhalilTest.java
+++ src/KhalilTest.java
@@ -2,5 +2,6 @@ public class KhalilTest {
     public static void main(String args[])
     {
         System.out.println("Hello I'm Khali , I'm Testing git ........................");
+        System.out.println("I'm Khalil");
     }
 }
